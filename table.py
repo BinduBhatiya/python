bcd1428418d245922ad72c32c12623d1cf84c2ba
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-14 17:25:25 +0530

    table

---
+++ table.py
@@ -0,0 +1,20 @@
+num = int(input("enter the number for print table: "))
+for i in range(1,11):
+    print(num,'*',i,'=',num*i)
+
+
+# output :
+'''
+        enter the number for print table: 11
+        
+        11 * 1 = 11
+        11 * 2 = 22
+        11 * 3 = 33
+        11 * 4 = 44
+        11 * 5 = 55
+        11 * 6 = 66
+        11 * 7 = 77
+        11 * 8 = 88
+        11 * 9 = 99
+        11 * 10 = 110
+'''
