fe16eff7ce8015c7445b8415394def53bdcfeda7
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-16 11:35:26 +0530

    triangle pattern

---
+++ triangle.py
@@ -0,0 +1,14 @@
+'''
+
+        *
+    *   *   *
+*   *   *   *   *
+
+'''
+
+for i in range(1,4):
+    for k in range(4-i):
+        print(' ',end=' ')
+    for j in range(i*2-1):
+        print('*',end=' ')
+    print('\n')
