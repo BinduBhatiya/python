a5ea8cc266f0a485a73efe5c089d4016583ed4d4
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-23 21:54:56 +0530

    any function

---
+++ List_exercise/any()_exercise/check_any_or_not.py
@@ -0,0 +1,18 @@
+# CHECK FOR THE VALUE IN LIST OR NOT USING "any()"
+
+# EXRCISE 1 :
+lst = [1,2,3,4,5]
+res = any(lst)
+print(res)                              # True
+
+# EXRCISE 2 :
+lst = []
+print(any(lst))                         # False
+
+# EXRCISE 3 :
+lst = [' ', 'hello', 'world']
+print(any(lst))                         # True
+
+# EXRCISE 4 :
+lst = [1,2,3,4,5]
+print(any(n > 5 for n in lst))           # False
