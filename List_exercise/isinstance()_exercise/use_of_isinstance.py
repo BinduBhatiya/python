7a7764bf27b66eac29ba7cc49c254ca05d86e3e2
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-23 21:51:30 +0530

    isinstance

---
+++ List_exercise/isinstance()_exercise/use_of_isinstance.py
@@ -0,0 +1,30 @@
+# SIMPLE USE OF ISINSTANCE() FUNCTION.
+
+# EXERCISE 1:
+
+x = 5
+print(isinstance(x, int))                                   # True
+
+
+# EXERCISE 2:
+
+lst = [1,2,3,4,5]
+print(isinstance(lst, (list,tuple)))                        # True
+
+
+# EXERCISE 3:
+
+class info:
+    pass
+obj = info()
+print(isinstance(obj,info))                                 # True
+
+
+# EXERCISE 4:
+
+class parent:
+    pass
+class derived(parent):
+    pass
+obj = derived()
+print(isinstance(obj,parent))                                 # True
