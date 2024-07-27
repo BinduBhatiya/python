723c5c09ae216f41ee78da3bbf9b98e127609e50
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-27 17:44:18 +0530

    dictionary method

---
+++ Dictionary/find_length_of_value.py
@@ -0,0 +1,34 @@
+# HOW TO FIND THE LENGTH OF VALUES IN DICTIONARY.
+
+# EXERCISE 1:-
+# USING SIMPLE FOR LOOP :
+# dic = {1:'python', 2:'is', 3:'best'}
+# lst = {}
+# for val in dic.values():
+#     lst[val] = len(val)
+# print(lst) 
+
+
+# {'python': 6, 'is': 2, 'best': 4}
+
+
+
+# EXERCISE 2:-
+# USING DICTIONARY COMPREHENSION :
+# dic = {1:'python', 2:'is', 3:'best'}
+# res = str({val: len(val) for val in dic.values()})
+# print(res) 
+
+
+# {'python': 6, 'is': 2, 'best': 4}
+
+
+
+# EXERCISE 3:-
+# USING LAMBDA,MAP :
+dic = {0:'python', 7:'is', 5:'best'}
+res = dict(map(lambda val: (val[1], len(val[1])), dic.items()))
+print(str(res)) 
+
+
+# {'python': 6, 'is': 2, 'best': 4}
