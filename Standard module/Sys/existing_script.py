5bdca620e6f79d0446cacb1882266560bb406e31
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-07-29 23:29:55 +0530

    sys module

---
+++ Standard module/Sys/existing_script.py
@@ -0,0 +1,7 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("No arguments provided, exiting...")
+    # sys.exit(1)  # Exit with a non-zero status to indicate an error
+else:
+    print("Arguments provided, continuing...")
---
+++ Standard module/Sys/get_version.py
@@ -0,0 +1,8 @@
+# getting version of sys
+
+import sys
+
+print("Python version:")
+print(sys.version)              # python 3.11.0
+
+print("Platform:", sys.platform)    # win32
