3f3d902cfa4764f0e6d648abf2e964cb587160f7
bindu bhatiya <bhatiyabindu81@gmail.com>
2024-08-09 23:06:23 +0530

    Triangle

--- triangle.py
+++ triangle.py
@@ -12,3 +12,19 @@ for i in range(1,4):
     for j in range(i*2-1):
         print('*',end=' ')
     print('\n')
+
+
+'''
+       *  
+
+     *   *  
+
+   *   *   *  
+'''
+
+for i in range(1,4):
+    for k in range(4-i):
+        print(' ',end=' ')
+    for j in range(i):
+        print(' * ',end=' ')
+    print('\n')
