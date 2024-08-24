20983f6209d35edff7fe37605a90893119a5d7bf
Bindu Bhatiya <bhatiyabindu81@gmail.com>
2024-08-24 22:29:24 +0530

    Generator

--- Generator/fibonacci.py
+++ Generator/fibonacci.py
@@ -4,9 +4,9 @@ def fib(n):
 
     for i in range(n):
         yield a
-        a, b = b, a+b
+        a,b = b,a+b
     
 obj = fib(15)
 for i in fib(10):
-    print(next(obj))
+    print(next(obj),end=" ")    # 0 1 1 2 3 5 8 13 21 34
     
