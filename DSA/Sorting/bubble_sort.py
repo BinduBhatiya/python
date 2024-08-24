4715507c1bf5b2592d97b2c8041ced4099071a83
Bindu Bhatiya <bhatiyabindu81@gmail.com>
2024-08-24 22:24:53 +0530

    Sorting

--- DSA/Sorting/bubble_sort.py
+++ DSA/Sorting/bubble_sort.py
@@ -6,4 +6,6 @@ for i in range(n):
     for j in range(n-i-1):
         if lst[j] > lst[j+1]:
             lst[j],lst[j+1] = lst[j+1],lst[j]
+            # lst[j] = lst[j+1]
+            # lst[j+1] = lst[j]
 print('after sorting : ',lst)
--- DSA/Sorting/insertion_sort.py
+++ DSA/Sorting/insertion_sort.py
@@ -5,7 +5,6 @@ def insertion(lst):
     for i in range(1, n):
         key = lst[i]
         j = i-1
-        # for j in range(0,n-1):
         while j >= 0 and lst[j] > key:
             lst[j+1] = lst[j]
             j -= 1
